128ef7541ffdb89545ccdb6157ad86d54f2c45ab
krishna vangala <currydeveloper@gmail.com>
2026-03-27 19:08:11 -0500

    Updated stuff.

--- ldapservice/ldapconnection.go
+++ ldapservice/ldapconnection.go
@@ -2,8 +2,21 @@ package ldapservice
 
 import (
 	"fmt"
+
+	ldap "github.com/go-ldap/ldap/v3"
 )
 
-func NewConnection() {
+func NewConnection(user string, password string, host string, port string) {
 	fmt.Println("Called from the new func")
+	l, err := ldap.DialURL("ldap://" + host + ":" + port)
+	if err != nil {
+		fmt.Println(fmt.Errorf("dial failed: %w", err))
+		return
+	}
+	defer l.Close()
+	fmt.Println("Entered the connection")
+	bind := ldap.SimpleBindRequest{Username: user, Password: password}
+	_, err = l.SimpleBind(&bind)
+	fmt.Println("Bind completed")
+
 }
--- mtea.go
+++ mtea.go
@@ -9,10 +9,13 @@ import (
 )
 
 func main() {
+	user := "uid=admin,ou=system"
+	host := "localhost"
+	port := "10389"
 	fmt.Println("Entering ldap client")
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter the password")
 	pwd, _ := reader.ReadString('\n')
 	fmt.Printf("Entered %v", pwd)
-	ldapservice.NewConnection()
+	ldapservice.NewConnection(user, pwd, host, port)
 }
